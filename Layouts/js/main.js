cb3d434f8b72655fa2d81b8a2e2b94e2b5186aee
Kazaryan Fyodor <69652931+da-blackest-funeral@users.noreply.github.com>
2022-01-15 14:35:27 +0300

    fix in main.js

--- Layouts/js/main.js
+++ Layouts/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     $('.show').click(function () {
-        $('#wrapper').load('http://192.168.56.10?page=' + $(this).attr('id'));
+        $('#wrapper').load('/?page=' + $(this).attr('id'));
     });
 });
